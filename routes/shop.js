2055465b44d57c540a4a8e6936084bdb979f30f7
AnushaYadaV4 <anu468mjl@gmail.com>
2023-03-30 12:02:30 +0530

    Dynamic route part 2-adding product to backend

--- routes/shop.js
+++ routes/shop.js
@@ -24,8 +24,6 @@ router.get('/', productsController.getProducts);
 module.exports = router;
 */
 
-const path = require('path');
-
 const express = require('express');
 
 const shopController = require('../controllers/shop');
@@ -36,9 +34,11 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-router.get('/products/:productId',shopController.getProduct)
+router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
 
 router.get('/orders', shopController.getOrders);
 
