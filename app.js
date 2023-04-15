6db09fca499826d05222ee22d39fe3fef82689d3
AnushaYadaV4 <anu468mjl@gmail.com>
2023-04-15 13:03:43 +0530

    connecting to database

--- app.js
+++ app.js
@@ -1,41 +1,13 @@
 
 
-/*const path = require('path');
 
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const contactRoutes=require('./routes/contactUs');
-const successRoutes=require('./routes/success')
-
-const errorController = require('./controllers/error');
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/admin', adminRoutes);
-app.use("/shop", shopRoutes);
-app.use('/contactUs',contactRoutes)
-app.use('/success',successRoutes)
-
-
-app.use(errorController.get404);
-
-app.listen(3000);
-*/
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db=require('./helper/database');
 
 const app = express();
 
@@ -44,6 +16,8 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
